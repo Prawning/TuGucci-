d84f7dba5aa288ba8f5d40a8f61d9b2b785b54c4
Jovian Lim <kiriketsuki@gmail.com>
2022-08-21 22:29:56 +0800

    added sentiment

--- src/views/Sentiment.vue
+++ src/views/Sentiment.vue
@@ -1,7 +1,8 @@
 <template>
     <div class = "w-screen h-screen text-5xl flex flex-col gap-10 items-center justify-center">
-        <div v-for="object in sentiments" :key="object">
-            {{object.time_created}}: {{object.score}}
+        <div class="chart_container">
+            <canvas id ="chartjs">
+            </canvas>
         </div>
     </div>
 </template>
@@ -10,9 +11,8 @@
     import {db, auth} from '../main.js';
     import {ref, onMounted} from 'vue';
     import {collection, addDoc, query, where, onSnapshot} from "firebase/firestore";
-    import axios from 'axios';
+    import Chart from 'chart.js/auto';
     const uuid = auth.currentUser.uid;
-
     const sentiments = ref([]);
 
     function load_sentiment_firestore() {
@@ -22,11 +22,85 @@
             const data = [];
             query_snapshot.forEach((doc) => {
                 var date = doc.data().date.toDate();
+                // convert to yy-mm-dd-hh-mm-ss
+                var date_string = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "/" + date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+                date = date_string;
+                // date = String(date.getTime())
+                // date = date.slice(date.length - 7, date.length - 1); // get last 6
                 var score = doc.data().score;
                 data.push({time_created: date, score: score});
             });
             sentiments.value = data;
+            redraw();
         });
+    }
+
+    function redraw() {
+        const ctx = document.querySelector('#chartjs').getContext('2d');
+        Chart.defaults.font.size = 20;
+        Chart.defaults.font.family = "Bio Rhyme";
+        Chart.defaults.color = "#fff";
+        const chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: sentiments.value.map((sentiment) => sentiment.time_created),
+                datasets: [{
+                    label: null,
+                    data: sentiments.value.map((sentiment) => sentiment.score),
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    tension: 0.1,
+                    spanGaps: true,
+                    borderWidth: 5,
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                scales: {
+                    x: {
+                        display: true,
+                        title: {
+                            display: true,
+                            text: 'Time'
+                        },
+                        grid: {
+                            borderColor: '#fff',
+                        }
+                    },
+
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Sentiment Score'
+                        },
+                        grid: {
+                            borderColor: '#fff',
+                        }
+                    }
+                },
+                plugins: {
+                    subtitle: {
+                        display: true,
+                        text: 'Sentiment over time',
+                        font: {
+                            size: 36,
+                        },
+                    },
+                    legend: {
+                        display: false,
+                    }
+                },
+                layout: {
+                    padding: {
+                        top: 50,
+                        bottom: 50,
+                        left: 50,
+                        right: 50,
+                    }
+                }
+            },
+        })
     }
 
     onMounted(() => {
@@ -34,3 +108,15 @@
         load_sentiment_firestore();
     })
 </script>
+
+<style scoped>
+    .chart_container {
+        @apply w-[70vw] h-[70vh] rounded-lg shadow-md;
+        background: rgba(0, 0, 0, 0.5);
+        backdrop-filter: blur(10px);
+    }
+
+    g {
+        @apply w-10;
+    }
+</style>
